15cd9974c55069a348e1bfee4eb7d5c2aa704bd7
Sahana Krishnan <sahanasr03@gmail.com>
2021-07-23 15:08:01 -0400

    Wrapper Class

--- Practice/src/app/wrapperClasses/StringExample.java
+++ Practice/src/app/wrapperClasses/StringExample.java
@@ -13,6 +13,15 @@ public class StringExample {
         String txt = "My name is \"Sahana\" and I am 18.";
         System.out.println(txt);
 
+
+        String s1="Javatpoint";    
+        String substr = s1.substring(0); // Starts with 0 and goes to end  
+        System.out.println(substr);  
+        String substr2 = s1.substring(5,10); // Starts from 5 and goes to 10  
+        System.out.println(substr2);    
+        //String substr3 = s1.substring(5,15); // Returns Exception 
+
+
     }
 
 }
